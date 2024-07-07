3eb86347ca2c338313cfcfd29d62c2968279fe23
Sergio Escudero Manzano <sergio.escudero.dev@gmail.com>
2024-07-07 11:00:14 +0200

    Initial Commit

---
+++ StartupNewsUITests/StartupNewsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StartupNewsUITestsLaunchTests.swift
+//  StartupNewsUITests
+//
+//  Created by Admin on 07/07/2024.
+//
+
+import XCTest
+
+final class StartupNewsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
